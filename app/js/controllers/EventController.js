cf91f9e10ac621bb45b159bb649641871df54a9d
sambarr7 <mynameissam@gmail.com>
2016-08-16 17:47:37 -0400

    Add files via upload

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope) {
+
+        $scope.event = {
+            name: 'Angular Boot Camp',
+            date: '8/16/16',
+            time: '5:30 pm'
+        }
+    }
+);
