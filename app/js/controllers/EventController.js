f7a7a3cac819258a384b91aad7d7fb4351ea7f12
sambarr7 <mynameissam@gmail.com>
2016-08-17 10:43:14 -0400

    Add files via upload

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -6,7 +6,36 @@ eventsApp.controller('EventController',
         $scope.event = {
             name: 'Angular Boot Camp',
             date: '8/16/16',
-            time: '5:30 pm'
+            time: '5:30 pm',
+            address: {
+                street: '123 Elm Street',
+                city: 'Miami',
+                state: 'Fl'
+            },
+            imageUrl: 'img/angularjs-logo.png',
+            sessions: [
+                {
+                    name: 'Directives Masterclass',
+                    creatorName: 'Bob Smith',
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    abstract: 'Learn about directives.'
+                },
+                {
+                    name: 'Scopes for fun and profit',
+                    creatorName: 'John McDon',
+                    duration: '30 min',
+                    level: 'Intro',
+                    abstract: 'Learn about scopes.'
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'Jane Doe',
+                    duration: '2 hrs',
+                    level: 'Intermediate',
+                    abstract: 'Learn about controllers.'
+                }
+            ]
         }
     }
 );
