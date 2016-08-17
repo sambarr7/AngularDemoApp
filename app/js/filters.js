8cd4dfcfd20ad8febd19aa04ca6f681df7eb6979
sambarr7 <mynameissam@gmail.com>
2016-08-17 17:37:29 -0400

    Add files via upload

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,1 +1,16 @@
 'use strict';
+
+eventsApp.filter('durFilter', function () {
+    return function (duration) {
+        switch (duration) {
+            case 1:
+                return "Half hour";
+            case 2:
+                return "One hour";
+            case 3:
+                return "Half day";
+            case 4:
+                return "Full day";
+        }
+    }
+});
